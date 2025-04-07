b53a7055a263edbf1cc0fac73db92e5638b150f2
ValentinZoia <valentinzoia@gmail.com>
2025-04-06 21:59:33 -0300

    first commit

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import './App.css'
+
+const App = () => {
+  return (
+    <div className='text-3xl text-primary bg-foreground font-bold underline'>
+      Hello Word!
+    </div>
+  )
+}
+
+export default App
