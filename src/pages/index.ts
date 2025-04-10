0953eb1707249c39945fa6848a36a337561ccc73
ValentinZoia <valentinzoia@gmail.com>
2025-04-10 20:16:23 -0300

    add(create pages components:home, aboutus, categories, error, login, products, register )

---
+++ src/pages/index.ts
@@ -0,0 +1,7 @@
+export * from './AboutUs';
+export * from './Categories';
+export * from './Error';
+export * from './Home';
+export * from './Login';
+export * from './Products';
+export * from './Register';
