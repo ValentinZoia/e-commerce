5b1964e7a23fd9bd538a9789a5891afd2cbaea52
ValentinZoia <valentinzoia@gmail.com>
2025-04-14 23:31:17 -0300

    refactory(add persist properties to CartState type)

--- src/types/cart.ts
+++ src/types/cart.ts
@@ -1,15 +1,20 @@
 import { Product } from "./product";
 
+
 export interface CartItem {
     productId: number;
     quantity: number;
     product?: Product; // Populated on client side
   }
   
-  export interface CartState {
+  export interface CartState  {
     items: CartItem[];
     totalItems: number;
     totalPrice: number;
     loading: boolean;
     error: string | null;
-  }
+    _persist?: { // <-- Añade esta propiedad opcional
+      version: number;
+      rehydrated: boolean;
+    };
+  } 
