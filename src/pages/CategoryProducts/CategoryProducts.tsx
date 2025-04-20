c830324e7b05a3e4f7aca5943894e8c40c3599cc
ValentinZoia <valentinzoia@gmail.com>
2025-04-20 00:11:00 -0300

    refactoty(add logic data to get categoryId from url parms categories/categoryId. In CategoryProducts.tsx)

--- src/pages/CategoryProducts/CategoryProducts.tsx
+++ src/pages/CategoryProducts/CategoryProducts.tsx
@@ -1,8 +1,18 @@
 
+import {useLoaderData} from 'react-router-dom'
 
 const CategoryProducts = () => {
+  const {categoryId} = useLoaderData() as{
+    categoryId: string
+  }
+  if(!categoryId){
+    
+    throw new Response("Invalid category ID", {status: 400})
+    
+  }
+
   return (
-    <div>CategoryProducts</div>
+    <div>{categoryId}</div>
   )
 }
 
