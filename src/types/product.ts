6217ea522a9526324e3fd5417c30c2a980961ede
ValentinZoia <valentinzoia@gmail.com>
2025-04-20 00:06:52 -0300

    refactory(add new properties in Product Type)

--- src/types/product.ts
+++ src/types/product.ts
@@ -3,11 +3,14 @@ import { Category } from "./category";
 
 export interface Product extends BaseEntity {
     name: string;
-    description: string;
+    description?: string;
     price: number;
+    discount?: number; //must be a number between 0 and 1
     stock: number;
     image: string;
     isFeatured?: boolean;
+    isNew?: boolean;
+    isPromotion?: boolean;
     categoryId: string;
     category?: Category;
   }
