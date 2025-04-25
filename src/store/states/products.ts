07b3c85b411a48e5e4c54a20baafeb56d8b56ee2
ValentinZoia <valentinzoia@gmail.com>
2025-04-24 23:45:25 -0300

    refactory(update initialState with new ProductState properties)

--- src/store/states/products.ts
+++ src/store/states/products.ts
@@ -1,12 +1,21 @@
 import { ProductsState } from "@/types";
 import { createSlice } from '@reduxjs/toolkit';
-import {fetchFeaturedProducts,fetchProductById,fetchProducts} from '@/utilities/productSlice';
+import {fetchFeaturedProducts,fetchProductById,fetchProducts,fetchPromotionalProducts,fetchNewProducts} from '@/utilities/productSlice';
 const initialState: ProductsState = {
     products: [],
     featuredProducts: [],
+    promotionalProducts: [],
+    newProducts:[],
     currentProduct: null,
     status: 'idle',
-    error: null
+    error: null,
+    lastFetched: {
+      all: undefined,
+      new: undefined,
+      featured: undefined,
+      promotion: undefined,
+      singleProduct: undefined
+    } 
   };
   
 
@@ -28,6 +37,7 @@ export const productsSlice = createSlice({
           .addCase(fetchProducts.fulfilled, (state, action) => {
             state.status = 'succeeded';
             state.products = action.payload;
+            state.lastFetched.all = Date.now(); 
           })
           .addCase(fetchProducts.rejected, (state, action) => {
             state.status = 'failed';
@@ -41,7 +51,41 @@ export const productsSlice = createSlice({
           .addCase(fetchFeaturedProducts.fulfilled, (state, action) => {
             state.status = 'succeeded';
             state.featuredProducts = action.payload;
+            state.lastFetched.featured = Date.now();
           })
+          .addCase(fetchFeaturedProducts.rejected, (state, action) => {
+            state.status = 'failed';
+            state.error = action.payload as string;
+          })
+
+          // Promotional Products
+          .addCase(fetchPromotionalProducts.pending, (state) => {
+            state.status = 'loading';
+          })
+          .addCase(fetchPromotionalProducts.fulfilled, (state, action) => {
+            state.status = 'succeeded';
+            state.promotionalProducts = action.payload;
+            state.lastFetched.promotion = Date.now();
+          })
+          .addCase(fetchPromotionalProducts.rejected, (state, action) => {
+            state.status = 'failed';
+            state.error = action.payload as string;
+          })
+
+           // New Products
+           .addCase(fetchNewProducts.pending, (state) => {
+            state.status = 'loading';
+          })
+          .addCase(fetchNewProducts.fulfilled, (state, action) => {
+            state.status = 'succeeded';
+            state.newProducts = action.payload;
+            state.lastFetched.new = Date.now();
+          })
+          .addCase(fetchNewProducts.rejected, (state, action) => {
+            state.status = 'failed';
+            state.error = action.payload as string;
+          })
+
           
           // Product by ID
           .addCase(fetchProductById.pending, (state) => {
@@ -50,6 +94,7 @@ export const productsSlice = createSlice({
           .addCase(fetchProductById.fulfilled, (state, action) => {
             state.status = 'succeeded';
             state.currentProduct = action.payload;
+            state.lastFetched.singleProduct = Date.now();
           })
           .addCase(fetchProductById.rejected, (state, action) => {
             state.status = 'failed';
