9e84d8e90aa99267dae4a57dded886be1639c564
ValentinZoia <valentinzoia@gmail.com>
2025-04-29 23:46:39 -0300

    refactory(used the skeleton in loadings states)

--- src/components/ProductsHeader/_components/ProductsCarousel/ProductsCarousel.tsx
+++ src/components/ProductsHeader/_components/ProductsCarousel/ProductsCarousel.tsx
@@ -8,6 +8,8 @@ import {
 import ProductCard from "@/components/Product/ProductCard";
 import { ProductStatus } from "@/types/product";
 import { useFetchProducts } from "@/hooks/useFetchProducts";
+import { ProductsCarouselSkeleton } from "@/components";
+import { EmptyState } from "@/components/EmptyState";
 
 interface ProductsCarouselProps {
   productsStatus: ProductStatus;
@@ -18,29 +20,33 @@ const ProductsCarousel = ({ productsStatus }: ProductsCarouselProps) => {
     productsStatus: productsStatus,
   });
 
-  if (status === "loading")
-    return <div className="text-center">Loading...</div>;
-  if (error) return <div className="text-center">Error: {error}</div>;
+  if (error) return <EmptyState title={`Error: ${error}`} />;
 
   return (
-    <>
-      <Carousel className="w-full">
-        <CarouselContent className="-ml-2 md:-ml-4">
-          {products.map((product) => (
+    <Carousel className="w-full">
+      <CarouselContent className="-ml-2 md:-ml-4">
+        {status === "loading" ? (
+          <ProductsCarouselSkeleton />
+        ) : !products || products.length === 0 ? (
+          <EmptyState title="No se encontraron productos"/>
+        ) : (
+          products.map((product) => (
             <CarouselItem
               key={product.id}
               className="pl-2 md:pl-4 md:basis-1/2 lg:basis-1/4"
             >
               <ProductCard product={product} productsStatus={productsStatus} />
             </CarouselItem>
-          ))}
-        </CarouselContent>
+          ))
+        )}
+      </CarouselContent>
+      {status !== "loading" && products && products.length > 0 && (
         <div className="flex justify-end gap-2 mt-4">
           <CarouselPrevious className="static transform-none" />
           <CarouselNext className="static transform-none" />
         </div>
-      </Carousel>
-    </>
+      )}
+    </Carousel>
   );
 };
 
