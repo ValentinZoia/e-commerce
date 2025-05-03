c0502d52779b6f2e40c93bf2e57c60984ba2f8e7
ValentinZoia <valentinzoia@gmail.com>
2025-05-03 00:23:21 -0300

    refactory(add a formatPrice function to utilities folder)

--- client/src/utilities/index.ts
+++ client/src/utilities/index.ts
@@ -1,3 +1,13 @@
 
 export * from './localstorage.utility';
 
+export const formatPrice = (price: number) => {
+    return new Intl.NumberFormat("es-AR", {
+      style: "currency",
+      currency: "ARS",
+      minimumFractionDigits: 2,
+      maximumFractionDigits: 2,
+    })
+      .format(price)
+      .replace(",00", ",00");
+  };
