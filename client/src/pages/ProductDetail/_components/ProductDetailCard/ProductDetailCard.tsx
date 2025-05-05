d7e70606a277f75ae9b2390c97ef4784da4abb79
ValentinZoia <valentinzoia@gmail.com>
2025-05-04 23:07:08 -0300

    fix(fix some bugs of conditional renders)

--- client/src/pages/ProductDetail/_components/ProductDetailCard/ProductDetailCard.tsx
+++ client/src/pages/ProductDetail/_components/ProductDetailCard/ProductDetailCard.tsx
@@ -1,8 +1,13 @@
 import { type Product } from "@/types";
 import { Image } from "@/components/Image";
-import { ChevronRight, RotateCcw, ShieldCheck} from "lucide-react";
+import {
+ 
+  ChevronRight,
+  RotateCcw,
+  ShieldCheck,
+} from "lucide-react";
 import { AddProductButton } from "@/components";
-import { Badge } from "@/components/ui/badge";
+
 import { cn } from "@/lib/utils";
 import {
   calculateItemCashPrice,
@@ -12,6 +17,7 @@ import { Link } from "react-router-dom";
 import { useState } from "react";
 import { formatPrice } from "@/utilities";
 import { Button } from "@/components/ui/button";
+import{ProductDetailImage} from '../ProductDetailImage'
 
 interface ProductDetailCardProps {
   product: Product;
@@ -21,6 +27,9 @@ const ProductDetailCard = ({ product }: ProductDetailCardProps) => {
   const [currentImageIndex, setCurrentImageIndex] = useState(0);
   const [selectedSize, setSelectedSize] = useState<string | null>(null);
   const [selectedStock, setSelectedStock] = useState<number | null>(null);
+  const isDiscount = product.discountPercentage === undefined || product.discountPercentage === 0 ? false : true;
+  
+  const isCashDiscount = product.cashDiscountPercentage === undefined || product.cashDiscountPercentage === 0 ? false : true;
 
   const handleMouseEnter = (index: number) => {
     if (product.images.length > 1) {
@@ -45,13 +54,16 @@ const ProductDetailCard = ({ product }: ProductDetailCardProps) => {
     }
     return null;
   };
+
+  const addProductButtonDisabled:boolean = (product.sizes ? selectedStock === 0 : false) || !product.sizes ? false : selectedSize === null
+
   return (
     <>
-      <div className="max-w-6xl mx-auto px-4">
+      <div className="max-w-6xl mx-auto   ">
         {product.categoryId && product.name && (
           <div className="flex items-center gap-1 text-sm mb-4">
             <div className="flex items-center">
-              <Link to={"/"} className="text-emerald-600 hover:underline">
+              <Link to={"/"} className="text-celeste hover:underline">
                 Inicio
               </Link>
               <ChevronRight className="h-3 w-3 mx-1 text-gray-400" />
@@ -59,7 +71,7 @@ const ProductDetailCard = ({ product }: ProductDetailCardProps) => {
             <div className="flex items-center">
               <Link
                 to={`/category/${product.categoryId.toLocaleLowerCase()}`}
-                className="text-emerald-600 hover:underline"
+                className="text-celeste hover:underline"
               >
                 {product.categoryId}
               </Link>
@@ -68,7 +80,7 @@ const ProductDetailCard = ({ product }: ProductDetailCardProps) => {
             <div className="flex items-center">
               <Link
                 to={`/products/${product.id}`}
-                className="text-emerald-600 hover:underline"
+                className="text-celeste hover:underline"
               >
                 {product.name}
               </Link>
@@ -80,6 +92,7 @@ const ProductDetailCard = ({ product }: ProductDetailCardProps) => {
           {/* Parte izquierda - Imagenes */}
           <div className="space-y-4">
             <div className="flex gap-4">
+              {/* VISTA DE IMAGENES PEQUEÑAS - SE VERA A PARTIR DE MD */}
               {product.images.length > 1 && (
                 <div className="hidden md:flex flex-col gap-2">
                   {product.images.slice(0, 4).map((image, index) => (
@@ -88,15 +101,15 @@ const ProductDetailCard = ({ product }: ProductDetailCardProps) => {
                       className={cn(
                         "w-20 h-20 border rounded cursor-pointer overflow-hidden",
                         currentImageIndex === index &&
-                          "border-emerald-500 border-2"
+                          "border-celeste border-2"
                       )}
                       // onClick={() => setCurrentImageIndex(index)}
-                      onMouseEnter={()=>handleMouseEnter(index)}
+                      onMouseEnter={() => handleMouseEnter(index)}
                     >
                       <Image
                         src={image}
                         alt={`${product.name} - Vista ${index + 1}`}
-                        placeholderSrc="caca"
+                        placeholderSrc="https://placehold.co/80"
                         width={80}
                         height={80}
                         className="object-cover w-full h-full"
@@ -108,28 +121,10 @@ const ProductDetailCard = ({ product }: ProductDetailCardProps) => {
                   ))}
                 </div>
               )}
+
+              {/* IMAGEN PRINCIPAL - SE VERA EN TODAS LAS RESOLUCIONES */}
               <div className="relative  w-full overflow-hidden rouded-md border">
-                <Image
-                  src={product.images[currentImageIndex]}
-                  alt={product.name}
-                  className="object-cover transition-transform duration-300"
-                  placeholderSrc="https://placehold.co/300"
-                  errorSrc="https://placehold.co/300"
-                  lazy={true}
-                  
-                  threshold={0.2}
-                />
-                {product.discountPercentage &&
-                  product.discountPercentage > 0 && (
-                    <Badge className="absolute top-3 right-3 bg-emerald-600 hover:bg-emerald-700">
-                      {product.discountPercentage * 100}% OFF
-                    </Badge>
-                  )}
-                {product.images.length > 1 && (
-                  <div className="absolute bottom-3 right-3 text-xs bg-white/80 px-2 py-1 rounded-full">
-                    {currentImageIndex + 1}/{product.images.length}
-                  </div>
-                )}
+                <ProductDetailImage product={product} currentImageIndex={currentImageIndex} setCurrentImageIndex={setCurrentImageIndex} isDiscounted={isDiscount}/>
               </div>
             </div>
           </div>
@@ -138,34 +133,50 @@ const ProductDetailCard = ({ product }: ProductDetailCardProps) => {
           <div className="space-y-6">
             {/* Datos del producto */}
             <div>
+              {/* Nombre */}
               <h1 className="text-2xl font-bold text-gray-800 mb-4">
                 {product.name}
               </h1>
 
+              {/* Precios y Descuentos */}
               <div className="space-y-2">
-                <div className="flex items-center gap-2">
-                  <span className="text-gray-500 line-through">
-                    {formatPrice(product.price)}
-                  </span>
+                {/* precio normal y con descuento fijo */}
+                <div className="flex flex-col items-start gap-1">
+                  {/* precio normal tachado si hay decuento aparece sino no */}
+                  {isDiscount && (
+                    <span className="text-gray-500 line-through">
+                      {formatPrice(product.price)}
+                    </span>
+                  )}
 
-                  <span className="text-2xl font-bold">
+                  {/* precio normal o con descuento fijo */}
+                  <div className="flex items-center gap-2">
+                  <span className="text-4xl ">
                     {formatPrice(calculateItemPrice(product))}
                   </span>
 
-                  {product.discountPercentage &&
-                    product.discountPercentage > 0 && (
-                      <Badge className="bg-emerald-600 hover:bg-emerald-700">
-                        -{product.discountPercentage * 100}%
-                      </Badge>
+                  { (isDiscount) && (
+                      <span className="text-celeste font-semilight text-lg">
+                         {product.discountPercentage && product.discountPercentage * 100}% OFF
+                      </span>
+                        
+                      
                     )}
+                  </div>
+                  
                 </div>
 
                 {/* Precio con descuento  por pagar en efectivo */}
-                {product.cashDiscountPercentage &&
-                  product.cashDiscountPercentage > 0 && (
-                    <p className="text-emerald-600 font-medium">
-                      {formatPrice(calculateItemCashPrice(product))} con
-                      Efectivo o Transferencia
+                {isCashDiscount && (
+                    <p className="text-celeste">
+                      <span className="text-2xl ">
+                        {formatPrice(calculateItemCashPrice(product))} {" "}
+                      </span>
+                      <span className="">
+                      con Efectivo o Transferencia
+                      
+                      </span>
+                       
                     </p>
                   )}
 
@@ -184,13 +195,18 @@ const ProductDetailCard = ({ product }: ProductDetailCardProps) => {
             {/* Envio gratis */}
             {product.isFreeShipping && !product.freeShippingThreshold && (
               <div className="flex items-center gap-2  ">
-                <span className="text-md bg-emerald-600 text-white px-4 py-1 rounded-md font-bold" >Envio gratis</span>
+                <span className="text-md bg-celeste text-white px-4 py-1 rounded-md font-bold">
+                  Envio gratis
+                </span>
               </div>
             )}
 
-            { !product.isFreeShipping && product.freeShippingThreshold  && (
-              <div className="flex items-center gap-2 text-emerald-600 text-sm">
-                <span>Envio gratis superando los {formatPrice(product.freeShippingThreshold)}</span>
+            {!product.isFreeShipping && product.freeShippingThreshold && (
+              <div className="flex items-center gap-2 text-celeste text-sm">
+                <span>
+                  Envio gratis superando los{" "}
+                  {formatPrice(product.freeShippingThreshold)}
+                </span>
               </div>
             )}
 
@@ -198,27 +214,30 @@ const ProductDetailCard = ({ product }: ProductDetailCardProps) => {
             {product.sizes && (
               <div className="space-y-2">
                 <div className="flex items-center gap-2 flex-wrap">
-                    <p className="font-medium">Talle: {selectedSize}</p>
-                    <p className="font-medium">Stock del talle: {selectedStock}</p>
+                  <p className="">Talle: <span className="font-medium">{selectedSize}</span></p>
+                  <p className="">
+                    Stock del talle: <span className="font-medium">{selectedStock}</span>
+                  </p>
                 </div>
-                
+
                 <div className="flex flex-wrap gap-2">
                   {product.sizes.map((size) => (
                     <Button
-                    key={size.name}
-                    variant={selectedSize === size.name ? "default" : "outline"}
-                    className={cn(
-                      "h-10 w-10 rounded-md cursor-pointer",
-                      selectedSize === size.name ? "bg-primary text-white" : "bg-white"
-                    )}
-                    onClick={() =>{
-                      setSelectedSize(size.name);
-                      setSelectedStock(size.stock);
-                    }}
-                    disabled={size.stock === 0}
+                      key={size.name}
+                      variant="outline"
+                      className={cn(
+                        "h-8 w-10 rounded-sm cursor-pointer border-[1px]",
+                        selectedSize === size.name
+                          ? "bg-gray-300 text-black border-black hover:bg-gray-300"
+                          : "bg-white"
+                      )}
+                      onClick={() => {
+                        setSelectedSize(size.name);
+                        setSelectedStock(size.stock);
+                      }}
+                      disabled={size.stock === 0}
                     >
                       {size.name}
-                      
                     </Button>
                   ))}
                 </div>
@@ -227,30 +246,35 @@ const ProductDetailCard = ({ product }: ProductDetailCardProps) => {
 
             {/* Agregar al carrito */}
             <div className="w-full flex items-center gap-4">
-              <AddProductButton 
+              <AddProductButton
                 product={product}
-                disabled={product.sizes ?selectedStock === 0 : false}
-                />
-              
+                disabled={addProductButtonDisabled}
+                size={selectedSize ? selectedSize : undefined}
+              />
             </div>
 
             {/* Politicas - Datos sugeridos - Etc */}
             <div className="space-y-4 pt-4 border-t">
-            <div className="flex items-start gap-3">
-              <ShieldCheck className="h-5 w-5 text-gray-700 mt-0.5" />
-              <div>
-                <p className="font-medium">Compra protegida</p>
-                <p className="text-sm text-gray-600">Tus datos cuidados durante toda la compra.</p>
+              <div className="flex items-start gap-3">
+                <ShieldCheck className="h-5 w-5 text-gray-700 mt-0.5" />
+                <div>
+                  <p className="font-medium">Compra protegida</p>
+                  <p className="text-sm text-gray-600">
+                    Tus datos cuidados durante toda la compra.
+                  </p>
+                </div>
+              </div>
+              <div className="flex items-start gap-3">
+                <RotateCcw className="h-5 w-5 text-gray-700 mt-0.5" />
+                <div>
+                  <p className="font-medium">Cambios y devoluciones</p>
+                  <p className="text-sm text-gray-600">
+                    Si no te gusta, contactanos y hacemos el cambio o la
+                    devolución.
+                  </p>
+                </div>
               </div>
             </div>
-            <div className="flex items-start gap-3">
-              <RotateCcw className="h-5 w-5 text-gray-700 mt-0.5" />
-              <div>
-                <p className="font-medium">Cambios y devoluciones</p>
-                <p className="text-sm text-gray-600">Si no te gusta, contactanos y hacemos el cambio o la devolución.</p>
-              </div>
-            </div>
-          </div>
           </div>
         </div>
       </div>
