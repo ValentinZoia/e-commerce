abc9318fc991ad0e28af6bef6c16f532b2851448
ValentinZoia <valentinzoia@gmail.com>
2025-05-04 23:04:44 -0300

    refacoty(change the background color of the Add Product Button)

--- client/src/components/AddProductButton/AddProductButton.tsx
+++ client/src/components/AddProductButton/AddProductButton.tsx
@@ -7,18 +7,21 @@ import { Button } from '../ui/button';
 interface AddProductButtonProps {
   product: Product
   disabled:boolean
+  size?:string
   
 }
 
-const AddProductButton = ({product, disabled}: AddProductButtonProps) => {
+const AddProductButton = ({product, disabled, size}: AddProductButtonProps) => {
   const dispatch = useAppDispatch();
   
     //Agregamos al carrito
     const handleAddToCart = (product: Product) => {
+      
       const data: CartItem = {
         productId: product.id,
         quantity: 1,
         product,
+        size
       };
   
       dispatch(addToCart(data));
@@ -30,7 +33,7 @@ const AddProductButton = ({product, disabled}: AddProductButtonProps) => {
       disabled={isOutOfStock || disabled}
       onClick={() => handleAddToCart(product)}
       
-      className="w-full bg-lime-500 hover:bg-lime-600 text-md text-black  h-10 px-12 rounded-none cursor-pointer">
+      className="w-full bg-addCart hover:bg-addCart/80 text-md text-black font-light  h-10 px-12 rounded-none cursor-pointer">
       <div className="flex items-center gap-2">
         <ShoppingCart className="w-6 h-6" />
         Agregar al carrito
