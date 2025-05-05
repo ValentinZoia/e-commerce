85c111ee9490ce01fd8cb05c3c1c252d0ec67c2f
ValentinZoia <valentinzoia@gmail.com>
2025-05-04 23:08:01 -0300

    refactory(create new design to CartPanel and CartItemCard)

--- client/src/components/ShoppingCart/CartPanel.tsx
+++ client/src/components/ShoppingCart/CartPanel.tsx
@@ -1,109 +1,155 @@
-import { useState, useEffect } from "react"
-import { ShoppingCart, Trash2 } from "lucide-react"
-import { Badge } from "@/components/ui/badge"
-import { Button } from "@/components/ui/button"
-import { DropdownMenu, DropdownMenuContent, DropdownMenuTrigger } from "@/components/ui/dropdown-menu"
+import { useState, useEffect } from "react";
+import {  ShoppingBag,  } from "lucide-react";
 
-import { Separator } from "@/components/ui/separator"
-import { RootState, useAppDispatch } from "@/store/store"
-import { useSelector } from "react-redux"
-import {CartItem} from '@/types/cart'
-import {removeItemFromCart, removeProductFromCart, clearCart} from '@/store/states/cart'
-import {CartItemCard} from './_components'
+import { Button } from "@/components/ui/button";
 
- 
+
+import { Separator } from "@/components/ui/separator";
+import { RootState, useAppDispatch } from "@/store/store";
+import { useSelector } from "react-redux";
+import { CartItem } from "@/types/cart";
+import {
+  removeItemFromCart,
+  removeProductFromCart,
+  clearCart,
+  plusItemFromCart,
+} from "@/store/states/cart";
+import { CartItemCard } from "./_components";
+import {
+  Sheet,
+  SheetContent,
+  SheetTrigger,
+  SheetHeader,
+  SheetTitle,
+} from "@/components/ui/sheet";
+import { formatPrice } from "@/utilities";
+
 
 const CartPanel = () => {
   const [cartItems, setCartItems] = useState<CartItem[]>([]);
 
-    const dispatch = useAppDispatch();
-    const { totalItems, totalPrice, items, loading, error } = useSelector((state:RootState) => state.cart);
+  const dispatch = useAppDispatch();
+  const { totalItems, totalPrice, items, loading, error } = useSelector(
+    (state: RootState) => state.cart
+  );
 
-    useEffect(() => {
-        setCartItems(items);
-      }, [items]);
-  
-    
+  useEffect(() => {
+    setCartItems(items);
+  }, [items]);
+
   //Vaciar carrito
-    const emptyCart = () => {
-        dispatch(clearCart());
-    };
+  const emptyCart = () => {
+    dispatch(clearCart());
+  };
 
-    //remover item individual
-    const removeItem = (productId: string) => {
-        dispatch(removeItemFromCart(productId));
-    };
+  //remover item individual
+  const removeItem = (productId: string) => {
+    dispatch(removeItemFromCart(productId));
+  };
 
-    //remover item completo
-    const removeProduct = (productId: string) => {
-        dispatch(removeProductFromCart(productId));
-    };
+  const plusItem = (productId: string) => {
+    dispatch(plusItemFromCart(productId));
+  }
 
+  //remover item completo
+  const removeProduct = (productId: string) => {
+    dispatch(removeProductFromCart(productId));
+  };
 
-  
-    if(loading) return <div>Loading...</div>
-    if(error) console.log(error);
-    
-  
+  if (loading) return <div>Loading...</div>;
+  if (error) console.log(error);
+
   return (
-        <DropdownMenu>
-          <DropdownMenuTrigger asChild>
-            <Button variant="ghost" size="icon" className="relative cursor-pointer">
-              <ShoppingCart className="h-5 w-5" />
-              {totalItems > 0 && (
-                <Badge className="absolute -top-2 -right-2 h-5 w-5 flex items-center justify-center p-0 text-xs">
-                  {totalItems}
-                </Badge>
-              )}
-              <span className="sr-only">Shopping Cart</span>
-            </Button>
-          </DropdownMenuTrigger>
-          <DropdownMenuContent align="end" className="w-80">
-            <div className="p-4">
-              <div className="flex items-center justify-between mb-4">
-                <h3 className="font-medium">Shopping Cart ({totalItems})</h3>
-                {cartItems.length > 0 && (
-                  <Button
-                    variant="ghost"
-                    size="sm"
-                    onClick={emptyCart}
-                    className="h-8 text-xs text-destructive hover:text-destructive"
-                  >
-                    <Trash2 className="h-4 w-4 mr-1" />
-                    Empty Cart
-                  </Button>
-                )}
-              </div>
     
-              {cartItems.length === 0 ? (
-                <div className="text-center py-6 text-muted-foreground">Your cart is empty</div>
-              ) : (
-                <>
-                  <div className="space-y-3 max-h-[300px] overflow-auto">
-                    {cartItems.map((item) => (
-                      <CartItemCard key={item.productId} item={item} removeItem={removeItem} removeProduct={removeProduct} />
-                    ))}
-                  </div>
-    
-                  <Separator className="my-4" />
-    
-                  <div className="flex items-center justify-between">
-                    <span className="font-medium">Total:</span>
-                    <span className="font-bold">${totalPrice.toFixed(0)}</span>
-                  </div>
-    
-                  <div className="mt-4 grid grid-cols-2 gap-2">
-                    <Button variant="outline" size="sm">
-                      View Cart
-                    </Button>
-                    <Button size="sm">Checkout</Button>
-                  </div>
-                </>
-              )}
+    <div className="p-6">
+      <Sheet>
+        <SheetTrigger asChild>
+          <Button
+            variant="ghost"
+            className="relative cursor-pointer hover:bg-transparent"
+          >
+            <div className="bg-lime-100 rounded-full p-2">
+              <ShoppingBag className="h-5 w-5 " />
             </div>
-          </DropdownMenuContent>
-        </DropdownMenu>
-      )
-}
+            <div className="flex flex-col items-start gap-0">
+              <span className="text-xs">Carrito ({totalItems})</span>
+              <span className="text-xs font-light">
+                {formatPrice(totalPrice)}
+              </span>
+            </div>
 
-export default CartPanel
+            <span className="sr-only">Shopping Cart</span>
+          </Button>
+        </SheetTrigger>
+
+        <SheetContent side="right" className="px-6">
+          <SheetHeader className="flex flex-row items-center justify-between">
+            <SheetTitle>Carrito de compras</SheetTitle>
+
+            {/* {cartItems.length > 0 && (
+              <Button
+                variant="ghost"
+                size="sm"
+                onClick={emptyCart}
+                className="h-8 cursor-pointer text-xs text-destructive hover:text-destructive"
+              >
+                <Trash2 className="h-4 w-4 mr-1" />
+                Vaciar carrito
+              </Button>
+            )} */}
+          </SheetHeader>
+
+          {cartItems.length === 0 ? (
+            <div className="text-center py-6 text-muted-foreground">
+              Tu carrito esta vacio
+            </div>
+          ) : (
+            <>
+              <div className="space-y-3 max-h-[300px] overflow-auto">
+                {cartItems.map((item) => (
+                  <CartItemCard
+                    key={item.productId}
+                    item={item}
+                    removeItem={removeItem}
+                    removeProduct={removeProduct}
+                    plusItem={plusItem}
+                  />
+                ))}
+              </div>
+
+              <Separator className="my-4" />
+
+              <div className="flex items-start justify-between">
+                <span className="">Total:</span>
+                <div className="flex flex-col items-end gap-0">
+                  <span className="text-lg">{formatPrice(totalPrice)}</span>
+                {cartItems[0].product?.cashDiscountPercentage &&
+                  cartItems[0].product?.cashDiscountPercentage > 0 && (
+                    <p className="text-celeste text-sm font-light">
+                      <span className=" ">
+                        O {formatPrice(
+                          totalPrice - cartItems[0].product?.cashDiscountPercentage * totalPrice)}
+                        {" "}
+                      </span>
+                      <span className="">con Efectivo o Transferencia</span>
+                    </p>
+                  )}
+                </div>
+                
+              </div>
+
+              {/* <div className=" w-full flex items-center justify-center"> */}
+                <Button variant="outline" className=" h-12 cursor-pointer text-md bg-addCart hover:bg-addCart/80 font-light" >
+                  Iniciar compra
+                </Button>
+                
+              {/* </div> */}
+            </>
+          )}
+        </SheetContent>
+      </Sheet>
+    </div>
+  );
+};
+
+export default CartPanel;
--- client/src/components/ShoppingCart/_components/CartItemCard/CartItemCard.tsx
+++ client/src/components/ShoppingCart/_components/CartItemCard/CartItemCard.tsx
@@ -1,61 +1,106 @@
 import { Button } from "@/components/ui/button";
 import { Card, CardContent } from "@/components/ui/card";
 import { CartItem } from "@/types";
-import { Trash2, X } from "lucide-react";
+import { Minus, Plus, X } from "lucide-react";
+import { Image } from "@/components/Image";
+import { formatPrice } from "@/utilities";
 
 
 interface CartItemProps {
   item: CartItem;
   removeItem: (productId: string) => void;
   removeProduct: (productId: string) => void;
+  plusItem: (productId: string) => void;
 }
 
-
-const CartItemCard = ({item, removeItem, removeProduct}: CartItemProps) => {
+const CartItemCard = ({ item, removeItem, removeProduct, plusItem }: CartItemProps) => {
+  const product = item.product;
+  if (!product) return null;
+  const isDiscount = product.discountPercentage !== undefined && product.discountPercentage > 0;
   return (
-    <Card key={item.productId} className="overflow-hidden">
-      <CardContent className="p-3">
-        <div className="flex items-center gap-3">
-          <div className="h-12 w-12 rounded overflow-hidden flex-shrink-0">
-            <img
-              src={item.product?.image || "/placeholder.svg"}
-              alt={item.product?.name}
-              className="h-full w-full object-cover"
+    <Card
+      key={item.productId}
+      className="overflow-hidden border-none shadow-none"
+    >
+      <CardContent className="p-0">
+        <div className="flex items-start justify-between gap-3">
+          <div
+            className={
+              "w-20 h-20 border rounded  overflow-hidden"
+            }
+          >
+            <Image
+              src={product.images[0]}
+              alt={`${product.name}`}
+              placeholderSrc="https://placehold.co/80"
+              width={80}
+              height={80}
+              className="object-cover w-full h-full"
+              lazy={true}
+              aspectRatio={1}
+              threshold={0.2}
             />
           </div>
-          <div className="flex-1 min-w-0">
-            <p className="text-sm font-medium truncate">{item.product?.name}</p>
-            <div className="flex items-center justify-between mt-1">
-              <div className="flex items-center gap-2">
-                <span className="text-sm font-semibold">
-                  ${item.product?.price.toFixed(0)}
-                </span>
-                <span className="text-xs text-muted-foreground">
-                  x{item.quantity}
-                </span>
-              </div>
-              <div className="flex items-center gap-1">
-                <Button
-                  variant="ghost"
-                  size="icon"
-                  className="h-6 w-6"
-                  onClick={() => removeItem(item.productId)}
-                >
-                  <Trash2 className="h-3 w-3" />
-                  <span className="sr-only">Remove one</span>
-                </Button>
-                <Button
-                  variant="ghost"
-                  size="icon"
-                  className="h-6 w-6 text-destructive"
-                  onClick={() => removeProduct(item.productId)}
-                >
-                  <X className="h-3 w-3" />
-                  <span className="sr-only">Remove all</span>
-                </Button>
-              </div>
+          <div className="flex-1 min-w-0 flex flex-col flex-wrap gap-2">
+            <p className="text-sm truncate max-w-50">
+              {item.product?.name} 
+            </p>
+            <p className="text-sm truncate">
+              {item.size && `(${item.size})`}
+            </p>
+            <div className="border-[1px] rounded-sm py-0.5 max-w-20 flex items-center justify-between ">
+              <Button
+                variant="ghost"
+                size="icon"
+                className="h-6 w-6 cursor-pointer"
+                onClick={() => removeItem(item.productId)}
+              >
+                <Minus className="h-3 w-3 font-light text-gray-500" />
+              </Button>
+              <span>{item.quantity}</span>
+              <Button
+                variant="ghost"
+                size="icon"
+                className="h-6 w-6 cursor-pointer"
+                onClick={() => plusItem(item.productId)}
+                
+              >
+                <Plus className="h-3 w-3 text-gray-500 " />
+              </Button>
             </div>
           </div>
+          <div className="flex flex-col items-end gap-1">
+            <Button
+              variant="ghost"
+              size="icon"
+              className="h-6 w-6 text-destructive"
+              onClick={() => removeProduct(item.productId)}
+            >
+              <X className="h-3 w-3" />
+              <span className="sr-only">Remove all</span>
+            </Button>
+            {/* precio normal tachado si hay decuento aparece sino no */}
+            {isDiscount && (
+              <div className="flex flex-rox gap-1">
+                <span className="text-celeste font-semilight text-xs">
+                  -{product.discountPercentage && product.discountPercentage * 100}%
+                </span>
+                <span className="text-gray-500 text-xs line-through">
+                  {formatPrice(product.price / (1 - (product.discountPercentage ? product.discountPercentage : 0)))}
+                </span>
+              </div>
+            )}
+
+            {/* precio normal o con descuento fijo */}
+            <div className="flex items-center gap-2">
+              <span className="text-md ">
+              {formatPrice(product.price)}
+              </span>
+
+              
+            </div>
+          </div>
+          
         </div>
       </CardContent>
     </Card>
