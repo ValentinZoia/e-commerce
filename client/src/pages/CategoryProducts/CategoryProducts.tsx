75be9e0d8ff284e2a9e7b349bd5dc94ad11f140c
ValentinZoia <valentinzoia@gmail.com>
2025-05-06 23:59:19 -0300

    refactory(use the custom hook useFetchCategories here)

--- client/src/pages/CategoryProducts/CategoryProducts.tsx
+++ client/src/pages/CategoryProducts/CategoryProducts.tsx
@@ -1,5 +1,9 @@
 
+import { useFetchProducts } from '@/hooks/useFetchProducts'
+import { ProductStatus } from '@/types'
 import {useLoaderData} from 'react-router-dom'
+import { ProductList } from '../Products/_components'
+import { EmptyState } from '@/components/EmptyState'
 
 const CategoryProducts = () => {
   const {categoryId} = useLoaderData() as{
@@ -11,8 +15,18 @@ const CategoryProducts = () => {
     
   }
 
+  const {status, error, products} = useFetchProducts({productsStatus: ProductStatus.FORCATEGORY, categoryId:categoryId.toLocaleLowerCase()});
+
+  if (status === "loading") return <div className="flex justify-center items-center h-screen text-center">Loading...</div>;
+  if (error) return <div className="flex justify-center items-center h-screen text-cente text-center">Error: {error}</div>;
+  if(products == undefined || products == null) return <EmptyState  title="No se encontraron productos"/>;
   return (
-    <div>{categoryId}</div>
+    <div className="container mx-auto py-10">
+      <div className="flex flex-col gap-4">
+        <ProductList products={products} title={`Productos de la categoria ${categoryId}`} />
+      </div>
+    </div>
+    
   )
 }
 
