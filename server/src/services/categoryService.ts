d09442261b771df0663e52338e347afed9b7d62d
ValentinZoia <valentinzoia@gmail.com>
2025-05-06 23:48:33 -0300

    refactory(delete unused functions)

--- server/src/services/categoryService.ts
+++ server/src/services/categoryService.ts
@@ -1,7 +1,7 @@
 
 import prisma from "../config/prisma";
 
-import {  Product, Category } from "../generated/prisma";
+import {   Category } from "../generated/prisma";
 
 
 
@@ -10,39 +10,7 @@ import {  Product, Category } from "../generated/prisma";
 export const CategoryService = {
     
 
-    async getProductsByCategory(categoryId: string): Promise<Product[]> {
-        try {
-            if (!categoryId) {
-                throw new Error("Category ID is required");
-            }
-            
-            const products = await prisma.product.findMany({
-                where: { categoryId },
-            });
-            
-            return products;
-        } catch (error) {
-            console.error(`Error in getProductsByCategory service for category ${categoryId}:`, error);
-            throw error;
-        }
-    },
-    async getProductsByCategoryV2(categoryId: string) {
-        try {
-            if (!categoryId) {
-                throw new Error("Category ID is required");
-            }
-
-            const category = await prisma.category.findMany({
-                where:{name:categoryId},
-                select:{products:true}
-            });
-            
-            return category;
-        } catch (error) {
-            console.error("Error in getProductsByCategoryV2 service:", error);
-            throw error;
-        }
-    },
+   
 
     async getAllCategories(): Promise<Category[]> {
         try {
