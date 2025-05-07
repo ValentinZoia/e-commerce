24dd99e9164d85050cce70a14f7894484262d089
ValentinZoia <valentinzoia@gmail.com>
2025-05-06 23:50:42 -0300

    refacotry(add categoryRouter in app.ts)

--- server/src/app.ts
+++ server/src/app.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import ProductRouter from './routes/product.route';
+import CategoryRouter from './routes/categories.route';
 // import productRoutes from './routes/productRoutes'
 
 const app = express();
@@ -11,5 +12,6 @@ app.use(express.json());
 
 //Routes
  app.use('/api/products', ProductRouter);
+ app.use('/api/categories', CategoryRouter);
 
 export default app;
