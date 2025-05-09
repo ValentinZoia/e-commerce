40671faa7a472f165900bb063a0dd7635f153fb8
ValentinZoia <valentinzoia@gmail.com>
2025-05-09 19:37:51 -0300

    feat(create the Filter type)

---
+++ client/src/types/filters.ts
@@ -0,0 +1,13 @@
+export interface Filters {
+    category?: string;
+    minPrice?: number;
+    maxPrice?: number;
+    inStock?: boolean;
+    freeShipping?: boolean;
+    size?: string;
+    isNew?: boolean;
+    isPromotion?: boolean;
+    isFeatured?: boolean;
+    searchQuery?: string;
+    sortBy?: 'price-asc' | 'price-desc' | 'newest' | 'best-discount';
+}
