54d16c6b2e856ecb6fb8c390699bcf48dcd26a01
ValentinZoia <valentinzoia@gmail.com>
2025-05-09 19:40:08 -0300

    refactory(use the usefilteredProducts hook and add the ProductFilters component to Products page and categoryProducts page)

--- client/src/pages/CategoryProducts/CategoryProducts.tsx
+++ client/src/pages/CategoryProducts/CategoryProducts.tsx
@@ -1,9 +1,12 @@
 
-import { useFetchProducts } from '@/hooks/useFetchProducts'
+
 import { ProductStatus } from '@/types'
 import {useLoaderData} from 'react-router-dom'
 import { ProductList } from '../Products/_components'
 import { EmptyState } from '@/components/EmptyState'
+import { useFilteredProducts } from '@/hooks/useFilteredProducts'
+import { ProductFilters } from '@/components/ProductFilters'
+import { capitalizeFirstLetter } from '@/utilities'
 
 const CategoryProducts = () => {
   const {categoryId} = useLoaderData() as{
@@ -15,16 +18,22 @@ const CategoryProducts = () => {
     
   }
 
-  const {status, error, products} = useFetchProducts({productsStatus: ProductStatus.FORCATEGORY, categoryId:categoryId.toLocaleLowerCase()});
+  const {status, error, products, filters, setFilters} = useFilteredProducts({productsStatus: ProductStatus.FORCATEGORY, categoryId:categoryId.toLocaleLowerCase()});
 
   if (status === "loading") return <div className="flex justify-center items-center h-screen text-center">Loading...</div>;
   if (error) return <div className="flex justify-center items-center h-screen text-cente text-center">Error: {error}</div>;
-  if(products == undefined || products == null) return <EmptyState  title="No se encontraron productos"/>;
+  if(products == undefined || products == null || products.length == 0) return <EmptyState  title="No se encontraron productos"/>;
   return (
-    <div className="container mx-auto py-10">
-      <div className="flex flex-col gap-4">
+    <div className="container px-0 py-10">
+      <div className='grid md:grid-cols-4'>
+        <div className='hidden md:block mr-4'>
+          <ProductFilters filters={filters} setFilters={setFilters} products={products} productsStatus={ProductStatus.FORCATEGORY} categoryId={capitalizeFirstLetter(categoryId)} />
+        </div>
+        <div className="col-span-3">
         <ProductList products={products} title={`Productos de la categoria ${categoryId}`} />
       </div>
+      </div>
+      
     </div>
     
   )
--- client/src/pages/Products/Products.tsx
+++ client/src/pages/Products/Products.tsx
@@ -1,8 +1,10 @@
 
 import { EmptyState } from "@/components/EmptyState";
 import { ProductList } from "./_components"
-import { useFetchProducts } from "@/hooks/useFetchProducts";
+
 import { ProductStatus } from "@/types";
+import { useFilteredProducts } from "@/hooks/useFilteredProducts";
+import { ProductFilters } from "@/components/ProductFilters";
 
 //En esta pagina se mostrarian todos los productos
 //Hay que hacer un fetching de datos
@@ -11,16 +13,22 @@ import { ProductStatus } from "@/types";
 
 
 const Products = () => {
-  const{status, error, products} = useFetchProducts({productsStatus: ProductStatus.All});
+  const {status, error, products, filters, setFilters} = useFilteredProducts({productsStatus: ProductStatus.All});
 
   if (status === "loading") return <div className="flex justify-center items-center h-screen text-center">Loading...</div>;
   if (error) return <div className="flex justify-center items-center h-screen text-cente text-center">Error: {error}</div>;
-  if(products == undefined || products == null) return <EmptyState  title="No se encontraron productos"/>;
+  if(products == undefined || products == null || products.length == 0) return <EmptyState  title="No se encontraron productos"/>;
   return (
-    <div className="container mx-auto py-10">
-      <div className="flex flex-col gap-4">
-        <ProductList products={products} title="Todos los productos" />
+    <div className="container px-0 py-10">
+      <div className='grid md:grid-cols-4 '>
+        <div className='hidden md:block mr-4'>
+          <ProductFilters filters={filters} setFilters={setFilters} products={products} productsStatus={ProductStatus.All} />
+        </div>
+        <div className="col-span-3">
+        <ProductList products={products} title={`Todos los productos`} />
       </div>
+      </div>
+      
     </div>
     
   )
