fffe0672aebde30626d984110914b03f5d9b2d0b
ValentinZoia <valentinzoia@gmail.com>
2025-05-27 16:55:06 -0300

    refactory(create a inputPassword )

--- client/src/components/ui/input.tsx
+++ client/src/components/ui/input.tsx
@@ -1,8 +1,8 @@
 import * as React from "react"
-
+import {Eye as EyeOpenIcon, EyeClosed as EyeClosedIcon } from "lucide-react";
 import { cn } from "@/lib/utils"
 
-function Input({ className, type, ...props }: React.ComponentProps<"input">) {
+function Input({ className, autoComplete, type, ...props }: React.ComponentProps<"input">) {
   return (
     <input
       type={type}
@@ -13,9 +13,58 @@ function Input({ className, type, ...props }: React.ComponentProps<"input">) {
         "aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive",
         className
       )}
+      autoComplete={autoComplete}
+      style={{
+        WebkitBackgroundClip: "text",
+      }}
       {...props}
     />
   )
 }
 
-export { Input }
+
+function InputPassword({ className, autoComplete, type, ...props }: React.ComponentProps<"input">) {
+  const [showPassword, setShowPassword] = React.useState(false);
+    const togglePasswordVisibility = () => setShowPassword(!showPassword);
+  return (
+    <div className={cn("relative ", className)}>
+    <input
+      type={type === "password" && showPassword ? "text" : type}
+      data-slot="input"
+      className={cn(
+        "file:text-foreground placeholder:text-muted-foreground selection:bg-primary selection:text-primary-foreground dark:bg-input/30 border-input flex h-9 w-full min-w-0 rounded-md border bg-transparent px-3 py-1 text-base shadow-xs transition-[color,box-shadow] outline-none file:inline-flex file:h-7 file:border-0 file:bg-transparent file:text-sm file:font-medium disabled:pointer-events-none disabled:cursor-not-allowed disabled:opacity-50 md:text-sm",
+        "focus-visible:border-ring focus-visible:ring-ring/50 focus-visible:ring-[3px]",
+        "aria-invalid:ring-destructive/20 dark:aria-invalid:ring-destructive/40 aria-invalid:border-destructive",
+        type==='password',
+        className
+      )}
+      autoComplete={autoComplete}
+      style={{
+        WebkitBackgroundClip: "text",
+      }}
+      {...props}
+    />
+    {type === "password" && (
+          <div className=" absolute inset-y-0 right-0 flex items-center pr-3">
+            {showPassword ? (
+              <EyeClosedIcon
+                className="cursor-pointer"
+                onClick={togglePasswordVisibility}
+                
+              />
+            ) : (
+              <EyeOpenIcon
+                className="cursor-pointer"
+                onClick={togglePasswordVisibility}
+                
+              />
+            )}
+            
+          </div>
+        )}
+      </div>
+  );
+}
+
+
+export { Input, InputPassword }
