b94b3e815843c914d91bb765696d738ddacd010c
ValentinZoia <valentinzoia@gmail.com>
2025-05-27 16:46:45 -0300

    feat(add a enums about PublicRoutes and PrivateRoutes)

--- client/src/types/index.ts
+++ client/src/types/index.ts
@@ -1,5 +1,7 @@
 export * from './baseTypes';
 export * from './cart';
 export * from './category';
+export * from './filters';
 export * from './product';
+export * from './route';
 export * from './shared';
---
+++ client/src/types/route.ts
@@ -0,0 +1,15 @@
+export enum PublicRoutes {
+    PRODUCTS = 'products',
+    CATEGORIES = 'categories',
+    NEW = 'new',
+    FEATURED = 'featured',
+    PROMOTION = 'promotion',
+    ABOUTUS = 'aboutus',
+    LOGIN = 'login',
+
+}
+
+export enum PrivateRoutes {
+    PRIVATE = 'private',
+    ADMIN = 'admin',
+}
