5b371b74af796461c060de0d5798bd377119de29
ValentinZoia <valentinzoia@gmail.com>
2025-05-27 16:50:19 -0300

    refactory(wrap the RouteProvider with React's Suspense)

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,9 +1,14 @@
-import './App.css'
-import {RouterProvider} from 'react-router-dom'
-import router from './routes'
+import "./App.css";
+import { RouterProvider } from "react-router-dom";
+import router from "./routes";
+import { Suspense } from "react";
 
 const App = () => {
-  return <RouterProvider router={router} />
-}
+  return (
+    <Suspense fallback={<>Cargando...</>}>
+      <RouterProvider router={router} />
+    </Suspense>
+  );
+};
 
-export default App
+export default App;
