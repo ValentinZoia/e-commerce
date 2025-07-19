74edb6eeeb2c0072a579cdfb583bd28d47e8f3d5
ValentinZoia <valentinzoia@gmail.com>
2025-07-18 23:36:53 -0300

    feat(add AppRouter with initial endpoints for products, categories, orders and Admin)

---
+++ server/src/shared/presentation/routes/AppRouter.route.ts
@@ -0,0 +1,29 @@
+import { CategoryRoutes } from '../../../Categories/presentation/routes';
+import { ProductRoutes } from '../../../Products/presentation/routes';
+import { AuthAdminRoutes } from '../../../Auth/presentation/routes';
+import { OrderRoutes } from '../../../Orders/presentation/routes/Order.route';
+import { Router } from 'express';
+
+
+
+
+export class AppRoutes {
+
+
+  static get routes(): Router {
+
+    const router = Router();
+
+    // Definir todas mis rutas principales
+    router.use('/api/admin', AuthAdminRoutes.routes )
+    router.use('/api/products', ProductRoutes.routes)
+    router.use('/api/categories', CategoryRoutes.routes)
+    router.use('/api/orders', OrderRoutes.routes)
+    
+
+
+    return router;
+  }
+
+
+}
