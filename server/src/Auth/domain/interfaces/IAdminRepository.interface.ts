ac2a955321dc8a41f89e56f2d53c484d0845accc
ValentinZoia <valentinzoia@gmail.com>
2025-07-18 23:01:05 -0300

    feat(create the IAdminRepository interface)

---
+++ server/src/Auth/domain/interfaces/IAdminRepository.interface.ts
@@ -0,0 +1,8 @@
+import { CreateAdminDto } from "../dtos/CreateAdmin.dto";
+import { Admin} from "../entities/Admin.entity";
+
+export interface IAdminRepository {
+    create(admin:CreateAdminDto):Promise<Admin>
+    
+    findAdminByUsername(username: string): Promise<Admin | null>;
+}
