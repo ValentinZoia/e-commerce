9408de756ff59998f7501cb39c9c49aada38f338
ValentinZoia <valentinzoia@gmail.com>
2025-08-01 17:36:28 -0300

    refactor(indent code)

--- server/src/Products/application/services/UpdateProduct.service.ts
+++ server/src/Products/application/services/UpdateProduct.service.ts
@@ -6,56 +6,64 @@ import { Service } from "../../../shared/application/base";
 import { CreateProductDto } from "../../domain/dtos";
 import { Product } from "../../domain/entities";
 
+export class UpdateProductService extends Service<
+  [string, CreateProductDto],
+  Product
+> {
+  constructor(
+    private readonly productRepository: IProductRepository,
+    private readonly categoryRepository: ICategoryRepository
+  ) {
+    super();
+  }
 
+  async execute(id: string, data: CreateProductDto): Promise<Product> {
+    // 2. Verificar que exista el producto a actualizar
+    const existingProduct = await this.productRepository.getById(id);
+    if (!existingProduct)
+      throw CustomError.notFound("Producto a actualizar no encontrado");
 
+    // 2. Verificar que exista la categoria del producto a crear
+    const existingCategory = await this.categoryRepository.getByName(
+      data.categoryId
+    );
+    if (!existingCategory)
+      throw new ValidationError({
+        categoryId: ["La categoría especificada no existe"],
+      });
 
+    //3. Verificar si el nombre ya existe en otro producto
+    const productWithSameName = await this.productRepository.getByName(
+      data.name
+    );
+    if (productWithSameName && existingProduct.id !== productWithSameName.id)
+      throw new ValidationError({
+        name: ["Ya existe un producto con ese nombre"],
+      });
 
+    //4. Actualizar producto
+    const newUpdatedProduct = new ProductBuilder()
+      .setId(null) //esto es intencional. En la db no se actualiza a null, sino que mantiene el id.
+      .setName(data.name)
+      .setDescription(data.description)
+      .setPrice(data.price)
+      .setDiscountPercentage(data.discountPercentage)
+      .setCashPrice(data.cashPrice)
+      .setCashDiscountPercentage(data.cashDiscountPercentage)
+      .setStock(data.stock)
+      .setSizes(data.sizes)
+      .setCurrentSize(data.currentSize)
+      .setFreeShippingThreshold(data.freeShippingThreshold)
+      .setIsFreeShipping(data.isFreeShipping)
+      .setIsFeatured(data.isFeatured)
+      .setIsPromotion(data.isPromotion)
+      .setIsNew(data.isNew)
+      .setCategoryId(data.categoryId)
+      .setInstallments(data.installments)
+      .setImages(data.images)
+      .setUpdatedAt(new Date())
+      .build();
 
-export class UpdateProductService extends Service<[string, CreateProductDto], Product> {
-    constructor(
-        private readonly productRepository: IProductRepository,
-        private readonly categoryRepository: ICategoryRepository,
-    ){
-        super();
-    }
-
-    async execute(id:string, data: CreateProductDto): Promise<Product> {
-        // 2. Verificar que exista el producto a actualizar
-        const existingProduct = await this.productRepository.getById(id);
-        if (!existingProduct) throw CustomError.notFound("Producto a actualizar no encontrado");
-
-        // 2. Verificar que exista la categoria del producto a crear
-        const existingCategory = await this.categoryRepository.getByName(data.categoryId);
-        if(!existingCategory) throw new ValidationError({categoryId:['La categoría especificada no existe']})
-
-        //3. Verificar si el nombre ya existe en otro producto
-        const productWithSameName = await this.productRepository.getByName(data.name);
-        if (productWithSameName && existingProduct.id !== productWithSameName.id) throw new ValidationError({name: ["Ya existe un producto con ese nombre"]});
-      
- 
-        //4. Actualizar producto
-        const newUpdatedProduct = new ProductBuilder()
-                    .setId(null)
-                    .setName(data.name)
-                    .setDescription(data.description)
-                    .setPrice(data.price)
-                    .setDiscountPercentage(data.discountPercentage)
-                    .setCashPrice(data.cashPrice)
-                    .setCashDiscountPercentage(data.cashDiscountPercentage)
-                    .setStock(data.stock)
-                    .setSizes(data.sizes)
-                    .setCurrentSize(data.currentSize)
-                    .setFreeShippingThreshold(data.freeShippingThreshold)
-                    .setIsFreeShipping(data.isFreeShipping)
-                    .setIsFeatured(data.isFeatured)
-                    .setIsPromotion(data.isPromotion)
-                    .setIsNew(data.isNew)
-                    .setCategoryId(data.categoryId)
-                    .setInstallments(data.installments)
-                    .setImages(data.images)
-                    .setUpdatedAt(new Date())
-                    .build();
-
-        return await this.productRepository.update(id, newUpdatedProduct);
-    }
+    return await this.productRepository.update(id, newUpdatedProduct);
+  }
 }
