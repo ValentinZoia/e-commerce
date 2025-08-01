ec6c4306cb712b5633d6fbb1aa11057cec7c0199
ValentinZoia <valentinzoia@gmail.com>
2025-08-01 17:30:17 -0300

    fix(fix error with id propertie from user)

--- server/src/Auth/application/services/CreateAdmin.service.ts
+++ server/src/Auth/application/services/CreateAdmin.service.ts
@@ -9,36 +9,32 @@ interface PublicUserResponse {
   username: string;
 }
 
-type HashFunction = (password:string) => string
-
-
-
+type HashFunction = (password: string) => string;
 
 export class CreateAdminService extends Service<
   [CreateAdminDto],
   PublicUserResponse
 > {
   constructor(
-      private adminRepository: IAdminRepository,
-      private readonly hashPassword: HashFunction = BcryptAdapter.hash,
-    ) {
+    private adminRepository: IAdminRepository,
+    private readonly hashPassword: HashFunction = BcryptAdapter.hash
+  ) {
     super();
   }
 
   async execute(createUserDto: CreateAdminDto): Promise<PublicUserResponse> {
-    const { username, password, id } = createUserDto;
+    const { username, password } = createUserDto;
 
     // 1. Validar que el username no este en uso
     const adminFound = await this.adminRepository.findAdminByUsername(username);
-    if (adminFound !== null) throw new ValidationError({ username: ["El username ya esta en uso"] });
-      
+    if (adminFound !== null)
+      throw new ValidationError({ username: ["El username ya esta en uso"] });
 
     // 2. Hashear contraseña
     const hashedPassword = this.hashPassword(password);
 
     //3. Crear admin
     const createdAdmin = await this.adminRepository.create({
-      id: id,
       username: username,
       password: hashedPassword,
     });
