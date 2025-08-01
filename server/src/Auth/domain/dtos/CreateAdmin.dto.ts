56b7c5f425a0537befdc988608c96d55d3d52a95
ValentinZoia <valentinzoia@gmail.com>
2025-08-01 17:31:59 -0300

    fix(fix error with id property from user)

--- server/src/Auth/domain/dtos/CreateAdmin.dto.ts
+++ server/src/Auth/domain/dtos/CreateAdmin.dto.ts
@@ -1,10 +1,14 @@
-import { z } from 'zod';
+import { z } from "zod";
 
 export const createAdminSchema = z.object({
-  username: z.string()
-    .min(3, 'Username debe tener al menos 3 caracteres')
-    .max(20, 'Username no puede exceder 20 caracteres')
-    .regex(/^[a-zA-Z0-9_]+$/, 'Username solo puede contener letras, números y guiones bajos')
+  username: z
+    .string()
+    .min(3, "Username debe tener al menos 3 caracteres")
+    .max(20, "Username no puede exceder 20 caracteres")
+    .regex(
+      /^[a-zA-Z0-9_]+$/,
+      "Username solo puede contener letras, números y guiones bajos"
+    )
     .trim()
     .refine((val) => val === val.toLowerCase(), {
       message: "El username debe estar en minúsculas.",
@@ -12,26 +16,25 @@ export const createAdminSchema = z.object({
     .refine((val) => !/\s/.test(val), {
       message: "El username no debe contener espacios.",
     }),
-    
-  
-  password: z.string()
-    .min(8, 'Password debe tener al menos 8 caracteres')
-    .regex(/^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)/, 'Password debe contener mayúscula, minúscula y número'),
-  
-  id: z.string().uuid().optional() // Si lo generas automáticamente
+
+  password: z
+    .string()
+    .min(8, "Password debe tener al menos 8 caracteres")
+    .regex(
+      /^(?=.*[a-z])(?=.*[A-Z])(?=.*\d)/,
+      "Password debe contener mayúscula, minúscula y número"
+    ),
+
+  // Si lo generas automáticamente
 });
 
 export type CreateAdminDTO = z.infer<typeof createAdminSchema>;
 
 export class CreateAdminDto {
-    private constructor(
-        public username: string,
-        public password: string,
-        public id?: string // Opcional si se genera automáticamente
-    ) {}
+  private constructor(public username: string, public password: string) {}
 
-    static create(data: unknown):  CreateAdminDto{
-        const parsedData = createAdminSchema.parse(data);
-        return new CreateAdminDto(parsedData.username, parsedData.password);
-    }
+  static create(data: unknown): CreateAdminDto {
+    const parsedData = createAdminSchema.parse(data);
+    return new CreateAdminDto(parsedData.username, parsedData.password);
+  }
 }
