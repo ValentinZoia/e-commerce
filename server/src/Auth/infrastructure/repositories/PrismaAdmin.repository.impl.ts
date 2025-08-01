70de12687bcfd8da87901da290ee74c17d53c3ba
ValentinZoia <valentinzoia@gmail.com>
2025-08-01 17:32:13 -0300

    fix(fix error with id property from user)

--- server/src/Auth/infrastructure/repositories/PrismaAdmin.repository.impl.ts
+++ server/src/Auth/infrastructure/repositories/PrismaAdmin.repository.impl.ts
@@ -6,50 +6,39 @@ import { Admin } from "../../domain/entities";
 
 export class PrismaAdminRepositoryImpl implements IAdminRepository {
   async create(admin: CreateAdminDTO): Promise<Admin> {
-    const { username, password, id } = admin;
-    
-      const createdAdmin = await prisma.admin.create({
-        data: {
-          id:id,
-          username: username,
-          password: password,
-        },
-      });
+    const { username, password } = admin;
 
-      //Mapear el objeto creado a la entidad Admin
-      return this.mapPrismaToAdmin(createdAdmin);
-    
-    
+    const createdAdmin = await prisma.admin.create({
+      data: {
+        username: username,
+        password: password,
+      },
+    });
+
+    //Mapear el objeto creado a la entidad Admin
+    return this.mapPrismaToAdmin(createdAdmin);
   }
 
   async findAdminByUsername(username: string): Promise<Admin | null> {
-    
-      const existingUser = await prisma.admin.findUnique({
-        where: { username },
-      });
-      if (!existingUser) return null;
+    const existingUser = await prisma.admin.findUnique({
+      where: { username },
+    });
+    if (!existingUser) return null;
 
-      //Mapear el objeto encontrado a la entidad Admin
-      return this.mapPrismaToAdmin(existingUser);
-    
+    //Mapear el objeto encontrado a la entidad Admin
+    return this.mapPrismaToAdmin(existingUser);
   }
 
   private mapPrismaToAdmin(object: { [key: string]: any }): Admin {
     const { id, _id, username, password } = object;
 
-    if (!_id || !id) throw new ValidationError({id: ["Missing id"]});
+    if (!_id && !id) throw new ValidationError({ id: ["Missing id"] });
 
-      
-    
+    if (!username)
+      throw new ValidationError({ username: ["Missing username"] });
 
-    if (!username) throw new ValidationError({username: ["Missing username"]});
-
-      
-    
-    if (!password) throw new ValidationError({password: ["Missing password"]});
-
-      
-    
+    if (!password)
+      throw new ValidationError({ password: ["Missing password"] });
 
     return new Admin(_id || id, username, password);
   }
