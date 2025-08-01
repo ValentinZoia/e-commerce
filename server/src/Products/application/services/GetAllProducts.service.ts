7f22f2bff0beb311ba1164a5237eeff7f4410abc
ValentinZoia <valentinzoia@gmail.com>
2025-08-01 17:35:58 -0300

    refactor(add a new step validation. Verify should 'take' and 'skip' doesnt be a negative)

--- server/src/Products/application/services/GetAllProducts.service.ts
+++ server/src/Products/application/services/GetAllProducts.service.ts
@@ -1,38 +1,37 @@
-import { IProductRepository } from "../../domain/interfaces";
-import { CustomError } from "../../../shared/domain/errors";
+import {
+  GetAllQueryOptions,
+  IProductRepository,
+} from "../../domain/interfaces";
 import { Product } from "../../domain/entities";
 import { Service } from "../../../shared/application/base";
+import { CustomError } from "../../../shared/domain/errors";
 
-interface Options {
-    category?: string;
-    featured?: boolean;
-    promotion?: boolean;
-    new?: boolean;
-    take?:number
-    skip?:number
-}
+export class GetAllProductsService extends Service<
+  [GetAllQueryOptions],
+  Product[]
+> {
+  constructor(private readonly productRepository: IProductRepository) {
+    super();
+  }
 
-export class GetAllProductsService extends Service<[Options], Product[]>{
-    constructor(
-        private readonly productRepository: IProductRepository,
-    ){
-        super();
+  async execute(options?: GetAllQueryOptions): Promise<Product[]> {
+    if (options?.take && options.take < 0) {
+      throw CustomError.badRequest("El valor de take debe ser mayor a 0");
     }
 
-    async execute(options?: Options): Promise<Product[]> {
-        const products = await this.productRepository.getAll({
-            category: options?.category,
-            featured: options?.featured,
-            promotion: options?.promotion,
-            new: options?.new,
-            take: options?.take,
-            skip: options?.skip
-        });
-
-        if (!products) throw CustomError.notFound("No se encontraron Productos");
-            
-        
-
-        return products;
+    if (options?.skip && options.skip < 0) {
+      throw CustomError.badRequest("El valor de skip debe ser mayor a 0");
     }
+
+    const products = await this.productRepository.getAll({
+      category: options?.category,
+      featured: options?.featured,
+      promotion: options?.promotion,
+      new: options?.new,
+      take: options?.take,
+      skip: options?.skip,
+    });
+
+    return products;
+  }
 }
