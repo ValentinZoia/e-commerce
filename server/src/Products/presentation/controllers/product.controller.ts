1af2b153ef9cd0f9a2d91ba5ed0276090d7727d1
ValentinZoia <valentinzoia@gmail.com>
2025-08-01 17:51:28 -0300

    refactor(add badRequest error when id or name is empty)

--- server/src/Products/presentation/controllers/product.controller.ts
+++ server/src/Products/presentation/controllers/product.controller.ts
@@ -1,27 +1,35 @@
-import { CreateProductService,UpdateProductService,GetAllProductsService,GetProductByNameService,GetProductByIdService, DeleteProductService } from "../../application/services";
+import {
+  CreateProductService,
+  UpdateProductService,
+  GetAllProductsService,
+  GetProductByNameService,
+  GetProductByIdService,
+  DeleteProductService,
+} from "../../application/services";
 import { Request, Response, NextFunction } from "express";
 import { CreateProductDto } from "../../domain/dtos";
-
+import { CustomError } from "../../../shared/domain/errors";
+import { GetAllQueryOptions } from "../../domain/interfaces";
 
 export class ProductController {
-    constructor(
-      private readonly createProductService: CreateProductService,
-      private readonly updateProductService: UpdateProductService,
-      private readonly deleteProductService: DeleteProductService,
-      private readonly getAllProductsService: GetAllProductsService,
-      private readonly getProductByIdService: GetProductByIdService,
-      private readonly getProductByNameService: GetProductByNameService,
-    ){}
+  constructor(
+    private readonly createProductService: CreateProductService,
+    private readonly updateProductService: UpdateProductService,
+    private readonly deleteProductService: DeleteProductService,
+    private readonly getAllProductsService: GetAllProductsService,
+    private readonly getProductByIdService: GetProductByIdService,
+    private readonly getProductByNameService: GetProductByNameService
+  ) {}
 
-    createProduct = async (
+  createProduct = async (
     req: Request,
     res: Response,
     next: NextFunction
   ): Promise<void> => {
     try {
-        const createProductDto = CreateProductDto.create(req.body);
-        const product = await this.createProductService.execute(createProductDto);
-        res.status(201).json(product);
+      const createProductDto = CreateProductDto.create(req.body);
+      const product = await this.createProductService.execute(createProductDto);
+      res.status(201).json(product);
     } catch (error) {
       next(error);
     }
@@ -34,8 +42,14 @@ export class ProductController {
   ): Promise<void> => {
     try {
       const { id } = req.params;
+      if (!id) {
+        throw CustomError.badRequest("Product id is required");
+      }
       const updateProductDto = CreateProductDto.create(req.body);
-      const updatedProduct = await this.updateProductService.execute(id, updateProductDto);
+      const updatedProduct = await this.updateProductService.execute(
+        id,
+        updateProductDto
+      );
       res.status(200).json(updatedProduct);
     } catch (error) {
       next(error);
@@ -49,8 +63,11 @@ export class ProductController {
   ): Promise<void> => {
     try {
       const { id } = req.params;
+      if (!id) {
+        throw CustomError.badRequest("Product id is required");
+      }
       await this.deleteProductService.execute(id);
-      res.status(204).json({ message: "Product deleted" }).end();
+      res.status(200).json({ message: "Product deleted" }).end();
     } catch (error) {
       next(error);
     }
@@ -70,7 +87,7 @@ export class ProductController {
         take,
         skip,
       } = req.query;
-      
+
       /*
       *la request se vera algo asi:
        - /api/products?featured=true.
@@ -80,21 +97,26 @@ export class ProductController {
        -/api/products?category=${categoryId}.
 
        dependiendo los productos que se quieran traer.
-      */ 
+      */
 
       const result = await this.getAllProductsService.execute({
         category: category as string,
-        featured: featured === 'true' ? true : featured === 'false' ? false : undefined,
-        new: isNew === 'true' ? true : isNew === 'false' ? false : undefined,
-        promotion:promotion === 'true' ? true :promotion === 'false' ? false : undefined,
+        featured:
+          featured === "true" ? true : featured === "false" ? false : undefined,
+        new: isNew === "true" ? true : isNew === "false" ? false : undefined,
+        promotion:
+          promotion === "true"
+            ? true
+            : promotion === "false"
+            ? false
+            : undefined,
         take: take ? parseInt(take as string) : undefined,
-        skip: skip ? parseInt(skip as string) : undefined
+        skip: skip ? parseInt(skip as string) : undefined,
       });
 
       res.status(200).json(result);
-
     } catch (error) {
-        next(error)
+      next(error);
     }
   };
 
@@ -105,7 +127,10 @@ export class ProductController {
   ): Promise<void> => {
     try {
       const { id } = req.params;
-      const product = await this.getProductByIdService.execute(id)
+      if (!id) {
+        throw CustomError.badRequest("Product id is required");
+      }
+      const product = await this.getProductByIdService.execute(id);
       res.status(200).json(product);
     } catch (error) {
       next(error);
@@ -119,11 +144,13 @@ export class ProductController {
   ): Promise<void> => {
     try {
       const { name } = req.params;
+      if (!name) {
+        throw CustomError.badRequest("Product name is required");
+      }
       const product = await this.getProductByNameService.execute(name);
       res.status(200).json(product);
     } catch (error) {
       next(error);
     }
   };
-
 }
