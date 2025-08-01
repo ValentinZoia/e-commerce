3231f3096635a95b7f625ee7e72057a56eb73f71
ValentinZoia <valentinzoia@gmail.com>
2025-08-01 17:52:20 -0300

    refactor(indent code)

--- server/src/Products/presentation/routes/product.route.ts
+++ server/src/Products/presentation/routes/product.route.ts
@@ -1,59 +1,75 @@
-import { CreateProductService,UpdateProductService,DeleteProductService,GetAllProductsService,GetProductByIdService,GetProductByNameService } from "../../application/services";
+import {
+  CreateProductService,
+  UpdateProductService,
+  DeleteProductService,
+  GetAllProductsService,
+  GetProductByIdService,
+  GetProductByNameService,
+} from "../../application/services";
 import { PrismaCategoryRepositoryImpl } from "../../../Categories/infrastructure/repositories";
 import { PrismaProductRepositoryImpl } from "../../infrastructure/repositories";
-import { ValidationMiddleware } from "../../../shared/presentation/middlewares";
+import {
+  ValidationMiddleware,
+  AuthMiddleware,
+} from "../../../shared/presentation/middlewares";
 import { ProductController } from "../controllers";
 import { productSchema } from "../../domain/dtos";
 import { Router } from "express";
-
-
+import { PrismaAdminRepositoryImpl } from "../../../Auth/infrastructure/repositories";
 
 export class ProductRoutes {
-    static get routes():Router {
-        const router = Router();
-        const productRepository = new PrismaProductRepositoryImpl();
-        const categoryRepository = new PrismaCategoryRepositoryImpl();
-        const createProductService = new CreateProductService(productRepository, categoryRepository);
-        const updateProductService = new UpdateProductService(productRepository, categoryRepository);
-        const deleteProductService = new DeleteProductService(productRepository);
-        const getAllProductsService = new GetAllProductsService(productRepository);
-        const getProductByIdService = new GetProductByIdService(productRepository);
-        const getProductByNameService = new GetProductByNameService(productRepository);
-        const controller = new ProductController(
-            createProductService,
-            updateProductService,
-            deleteProductService,
-            getAllProductsService,
-            getProductByIdService,
-            getProductByNameService
-        );
+  static get routes(): Router {
+    const router = Router();
+    const productRepository = new PrismaProductRepositoryImpl();
+    const categoryRepository = new PrismaCategoryRepositoryImpl();
+    const adminRepository = new PrismaAdminRepositoryImpl();
+    const createProductService = new CreateProductService(
+      productRepository,
+      categoryRepository
+    );
+    const updateProductService = new UpdateProductService(
+      productRepository,
+      categoryRepository
+    );
+    const deleteProductService = new DeleteProductService(productRepository);
+    const getAllProductsService = new GetAllProductsService(productRepository);
+    const getProductByIdService = new GetProductByIdService(productRepository);
+    const getProductByNameService = new GetProductByNameService(
+      productRepository
+    );
+    const controller = new ProductController(
+      createProductService,
+      updateProductService,
+      deleteProductService,
+      getAllProductsService,
+      getProductByIdService,
+      getProductByNameService
+    );
+    const authMiddleware = new AuthMiddleware(adminRepository);
 
-        //a estas rutas solo deberan acceder los admins, por eso se verifica con el middleware su session.
+    //a estas rutas solo deberan acceder los admins, por eso se verifica con el middleware su session.
     router.post(
-        '/',
-        // authMiddleware.authenticate,
-        ValidationMiddleware.validateBody(productSchema),
-        controller.createProduct
+      "/",
+      authMiddleware.authenticate,
+      ValidationMiddleware.validateBody(productSchema),
+      controller.createProduct
     );
     router.put(
-        '/:id',
-        // authMiddleware.authenticate,
-        ValidationMiddleware.validateBody(productSchema),
-        controller.updateProduct
+      "/:id",
+      authMiddleware.authenticate,
+      ValidationMiddleware.validateBody(productSchema),
+      controller.updateProduct
     );
     router.delete(
-        '/:id',
-        // authMiddleware.authenticate,
-        controller.deleteProduct
+      "/:id",
+      authMiddleware.authenticate,
+      controller.deleteProduct
     );
 
-    router.get('/', controller.getAllProducts);
-    router.get('/:id', controller.getProductById);
-    router.get('/name/:name', controller.getProductByName);
+    router.get("/", controller.getAllProducts);
+    router.get("/:id", controller.getProductById);
+    router.get("/name/:name", controller.getProductByName);
 
     return router;
-    }
-
-    
-    
+  }
 }
