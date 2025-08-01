c64bcb4de99391c9ec96e904fef5315cf7373806
ValentinZoia <valentinzoia@gmail.com>
2025-08-01 17:43:45 -0300

    refactor(create a GetAllQueryOptions interface )

--- server/src/Products/domain/interfaces/Iproduct.repository.ts
+++ server/src/Products/domain/interfaces/Iproduct.repository.ts
@@ -1,19 +1,19 @@
+import { Product } from "../entities";
 
-import {Product} from '../entities'
+export interface GetAllQueryOptions {
+  category?: string;
+  featured?: boolean;
+  promotion?: boolean;
+  new?: boolean;
+  take?: number;
+  skip?: number;
+}
 
 export interface IProductRepository {
-    create(product:Product): Promise<Product>;
-    update(id:string, product:Product): Promise<Product>;
-    delete(id:string): Promise<void>;
-    getById(id:string): Promise<Product | null>;
-    getByName(name:string): Promise<Product | null>
-    getAll(options?:{   
-        category?:string,
-        featured?:boolean,
-        promotion?:boolean,
-        new?:boolean,
-        skip?:number,
-        take?:number
-    }): Promise<Product[]>;
-    
+  create(product: Product): Promise<Product>;
+  update(id: string, product: Product): Promise<Product>;
+  delete(id: string): Promise<void>;
+  getById(id: string): Promise<Product | null>;
+  getByName(name: string): Promise<Product | null>;
+  getAll(options?: GetAllQueryOptions): Promise<Product[]>;
 }
