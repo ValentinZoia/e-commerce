ec50947ae17dfd7586245f05d36a3204b5e56b2b
ValentinZoia <valentinzoia@gmail.com>
2025-08-05 18:39:46 -0300

    refactor(indent code)

--- server/src/Categories/domain/dtos/CategoryData.dto.ts
+++ server/src/Categories/domain/dtos/CategoryData.dto.ts
@@ -1,28 +1,27 @@
-import {z} from 'zod';
+import { z } from "zod";
 
 export const categorySchema = z.object({
-  name: z.string().min(3, { message: 'El nombre debe tener al menos 3 caracteres' }),
-  slug: z.string()
-    .min(3, { message: 'El slug debe tener al menos 3 caracteres' }),
-  description: z.string().nullable().optional(),
+  name: z
+    .string()
+    .min(3, { message: "El nombre debe tener al menos 3 caracteres" }),
+  slug: z
+    .string()
+    .min(3, { message: "El slug debe tener al menos 3 caracteres" }),
+  description: z.string().nullable(),
 });
 
 export type CategoryDataDTO = z.infer<typeof categorySchema>;
 
 export class CategoryDataDto {
-    private constructor(
-        public name:string,
-        public slug:string,
-        public description: string | null | undefined
-    ){}
+  private constructor(
+    public name: string,
+    public slug: string,
+    public description: string | null
+  ) {}
 
-    static create(data: unknown): CategoryDataDto{
-        const parsedData = categorySchema.parse(data);
-        const {name, slug, description} = parsedData;
-        return new CategoryDataDto(
-            name,
-            slug,
-            description
-        );
-    }
+  static create(data: unknown): CategoryDataDto {
+    const parsedData = categorySchema.parse(data);
+    const { name, slug, description } = parsedData;
+    return new CategoryDataDto(name, slug, description);
+  }
 }
