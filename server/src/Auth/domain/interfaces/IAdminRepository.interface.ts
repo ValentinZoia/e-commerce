58d411aae696b28427c9ace0dfe1b8d49f2999b7
ValentinZoia <valentinzoia@gmail.com>
2025-08-05 18:37:07 -0300

    refactory(add new method 'deleteByUsername' in IAdminRepository interface)

--- server/src/Auth/domain/interfaces/IAdminRepository.interface.ts
+++ server/src/Auth/domain/interfaces/IAdminRepository.interface.ts
@@ -1,8 +1,10 @@
 import { CreateAdminDto } from "../dtos/CreateAdmin.dto";
-import { Admin} from "../entities/Admin.entity";
+import { Admin } from "../entities/Admin.entity";
 
 export interface IAdminRepository {
-    create(admin:CreateAdminDto):Promise<Admin>
-    
-    findAdminByUsername(username: string): Promise<Admin | null>;
+  create(admin: CreateAdminDto): Promise<Admin>;
+
+  findAdminByUsername(username: string): Promise<Admin | null>;
+
+  deleteByUsername(username: string): Promise<void>;
 }
