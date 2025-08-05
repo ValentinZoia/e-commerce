75dc6cdbd6369ab5c3b4f045381beea19fba4f66
ValentinZoia <valentinzoia@gmail.com>
2025-08-05 18:40:28 -0300

    refactor(change all types CategoryDataDto to Category)

--- server/src/Categories/domain/interfaces/ICategoryRepository.interface.ts
+++ server/src/Categories/domain/interfaces/ICategoryRepository.interface.ts
@@ -1,11 +1,10 @@
-import { CategoryDataDto } from "../dtos";
 import { Category } from "../entities";
 
 export interface ICategoryRepository {
-    create(category:CategoryDataDto): Promise<Category>;
-    update(id:string, category:CategoryDataDto): Promise<Category>;
-    delete(id:string): Promise<void>;
-    getAll(): Promise<Category[]>;
-    getById(categoryId:string): Promise<Category | null>;
-    getByName(name:string): Promise<Category | null>;
+  create(category: Category): Promise<Category>;
+  update(id: string, category: Category): Promise<Category>;
+  delete(id: string): Promise<void>;
+  getAll(): Promise<Category[]>;
+  getById(categoryId: string): Promise<Category | null>;
+  getByName(name: string): Promise<Category | null>;
 }
