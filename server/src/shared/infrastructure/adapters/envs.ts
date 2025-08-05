12bfc41f913eeb0d11acac8d96600b4c64a4b900
ValentinZoia <valentinzoia@gmail.com>
2025-08-05 18:32:00 -0300

    refactor(add new envs)

--- server/src/shared/infrastructure/adapters/envs.ts
+++ server/src/shared/infrastructure/adapters/envs.ts
@@ -1,12 +1,13 @@
-import 'dotenv/config';
-import {get} from 'env-var';
+import "dotenv/config";
+import { get } from "env-var";
 
-export const envs ={
-    PORT: get('PORT').required().asPortNumber(),
-    JWT_SECRET: get('JWT_SECRET').required().asString(),
-    CORS_ORIGIN: get('CORS_ORIGIN').required().asString(),
-    SALT_ROUNDS: get('SALT_ROUNDS').default('10').asIntPositive(),
-    DATABASE_URL: get('DATABASE_URL').required().asString(),
-    NODE_ENV: get('NODE_ENV').default('development').asString(),
-}
-
+export const envs = {
+  PORT: get("PORT").required().asPortNumber(),
+  JWT_SECRET: get("JWT_SECRET").required().asString(),
+  CORS_ORIGIN: get("CORS_ORIGIN").required().asString(),
+  SALT_ROUNDS: get("SALT_ROUNDS").default("10").asIntPositive(),
+  DATABASE_URL_PROD: get("DATABASE_URL_PROD").required().asString(),
+  DATABASE_URL_DEV: get("DATABASE_URL_DEV").required().asString(),
+  DATABASE_URL_TEST: get("DATABASE_URL_TEST").required().asString(),
+  NODE_ENV: get("NODE_ENV").default("development").asString(),
+};
