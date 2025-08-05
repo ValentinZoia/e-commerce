ef06474e3e3cda701402622fd56294db5c9871bd
ValentinZoia <valentinzoia@gmail.com>
2025-08-05 18:45:50 -0300

    refactor(create news mocks datas)

--- server/tests/helpers/factories/category-mocks.ts
+++ server/tests/helpers/factories/category-mocks.ts
@@ -1,6 +1,7 @@
+import { CategoryDataDto } from "../../../src/Categories/domain/dtos";
 import { Category } from "../../../src/Categories/domain/entities";
 
-export const mockCategoryData: Category = {
+export const mockValidCategoryDataResponse: Category = {
   id: "category-test-id",
   name: "category-test-id",
   slug: "category-test-id",
@@ -8,3 +9,25 @@ export const mockCategoryData: Category = {
   createdAt: new Date(),
   updatedAt: new Date(),
 };
+
+export const mockValidCategoryDataResponseV2: Category = {
+  id: "category-test-id-2",
+  name: "category-test-id-2",
+  slug: "category-test-id-2",
+  description: "category-test-id-2",
+  createdAt: new Date(),
+  updatedAt: new Date(),
+};
+
+export const mockValidCategoryDataRequest: CategoryDataDto = {
+  name: "category-test-id",
+  slug: "category-test-id",
+  description: "category-test-id",
+};
+
+export const mockValidCategoryDataRequestToCategory = new Category(
+  null, //id
+  "category-test-id", //name
+  "category-test-id", //slug
+  "category-test-id" //description
+);
