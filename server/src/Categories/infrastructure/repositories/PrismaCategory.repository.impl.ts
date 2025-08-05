c3df5f72d2f77f2e15930817aa13ba87f22970eb
ValentinZoia <valentinzoia@gmail.com>
2025-08-05 18:42:25 -0300

    refactor(change mapPrismaToCategory type parameter from 'any' to 'CategoryPrisma')

--- server/src/Categories/infrastructure/repositories/PrismaCategory.repository.impl.ts
+++ server/src/Categories/infrastructure/repositories/PrismaCategory.repository.impl.ts
@@ -1,75 +1,67 @@
 import { ICategoryRepository } from "../../domain/interfaces";
 import prisma from "../../../shared/infrastructure/database/prismaClient";
-import { CategoryDataDto } from "../../domain/dtos";
+
 import { Category } from "../../domain/entities";
+import { Category as CategoryPrisma } from "../../../generated/prisma";
 
 export class PrismaCategoryRepositoryImpl implements ICategoryRepository {
-  
-    async create(category: CategoryDataDto): Promise<Category> {
-        const createdCategory = await prisma.category.create({
-            data:{
-                name: category.name,
-                slug: category.slug,
-                description: category.description,
-                
-            },
+  async create(category: Category): Promise<Category> {
+    const createdCategory: CategoryPrisma = await prisma.category.create({
+      data: {
+        name: category.name,
+        slug: category.slug,
+        description: category.description,
+      },
+    });
+    return this.mapPrismaToCategory(createdCategory);
+  }
 
-            
-        });
-        return this.mapPrismaToCategory(createdCategory)
-    }
+  async delete(id: string): Promise<void> {
+    await prisma.category.delete({
+      where: { id },
+    });
+  }
 
-    async delete(id: string): Promise<void> {
-        await prisma.category.delete({
-            where:{id}
-        })
-    }
+  async update(id: string, category: Category): Promise<Category> {
+    const updatedCategory: CategoryPrisma = await prisma.category.update({
+      where: { id },
+      data: {
+        name: category.name,
+        slug: category.slug,
+        description: category.description,
+      },
+    });
+    return this.mapPrismaToCategory(updatedCategory);
+  }
 
-    async update(id: string, category: CategoryDataDto): Promise<Category> {
-        const updatedCategory = await prisma.category.update({
-            where:{id},
-            data:{
-                name: category.name,
-                slug: category.slug,
-                description: category.description,
-                
-            },
+  async getAll(): Promise<Category[]> {
+    const categories: CategoryPrisma[] = await prisma.category.findMany({
+      orderBy: { name: "asc" },
+    });
 
-            
-        });
-        return this.mapPrismaToCategory(updatedCategory)
-    }
-    
+    return categories.map((category) => this.mapPrismaToCategory(category));
+  }
 
-    async getAll(): Promise<Category[]> {
-        const categories = await prisma.category.findMany({
-            orderBy: {name:'asc'},
-        })
+  async getById(categoryId: string): Promise<Category | null> {
+    const category: CategoryPrisma | null = await prisma.category.findUnique({
+      where: { id: categoryId },
+    });
+    if (!category) return null;
 
-        return categories.map((category) => this.mapPrismaToCategory(category));
-    }
+    return this.mapPrismaToCategory(category);
+  }
 
+  async getByName(name: string): Promise<Category | null> {
+    const category: CategoryPrisma | null = await prisma.category.findUnique({
+      where: { name },
+    });
+    if (!category) return null;
 
-    async getById(categoryId: string): Promise<Category | null> {
-     const category = await prisma.category.findUnique({
-        where:{id : categoryId},
-     });
-     if(!category) return null;   
-
-     return this.mapPrismaToCategory(category);
-    }
-
-    async getByName(name: string): Promise<Category | null> {
-        const category = await prisma.category.findUnique({
-        where:{name},
-     });
-     if(!category) return null;   
-
-     return this.mapPrismaToCategory(category);
-    }
+    return this.mapPrismaToCategory(category);
+  }
 
   // Mappers para convertir entre modelos de Prisma y dominio
-  private mapPrismaToCategory(prismaCategory: any): Category {
+  private mapPrismaToCategory(prismaCategory: CategoryPrisma): Category {
     return new Category(
       prismaCategory.id,
       prismaCategory.name,
