7aaffdb1b9a201a7b04b53dc6d21dc6ef555895e
ValentinZoia <valentinzoia@gmail.com>
2025-08-08 20:59:49 -0300

    refactor(indent code)

--- server/src/Orders/application/services/GetOrdersByCustomerName.service.ts
+++ server/src/Orders/application/services/GetOrdersByCustomerName.service.ts
@@ -3,16 +3,18 @@ import { CustomError } from "../../../shared/domain/errors";
 import { Service } from "../../../shared/application/base";
 import { Order } from "../../domain/entities/Order.entity";
 
+export class GetOrdersByCustomerNameService extends Service<[string], Order[]> {
+  constructor(private orderRepository: IOrderRepository) {
+    super();
+  }
 
-export class GetOrdersByCustomerNameService extends Service<[string], Order[]>{
-    constructor(private orderRepository: IOrderRepository){
-        super()
-    }
+  async execute(customerName: string): Promise<Order[]> {
+    const order = await this.orderRepository.findByCustomerName(customerName);
+    if (!order)
+      throw CustomError.notFound(
+        `No se encontraron ningunas ordenes para el cliente ${customerName}   `
+      );
 
-    async execute(customerName: string): Promise<Order[]> {
-        const order = await this.orderRepository.findByCustomerName(customerName);
-        if(!order) throw  CustomError.notFound(`No se encontraron ningunas ordenes para el ciente ${customerName}   `);
-
-        return order;
-    }
+    return order;
+  }
 }
