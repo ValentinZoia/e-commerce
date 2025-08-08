a4c60867037d472b22cf1bce412c93bd3da00395
ValentinZoia <valentinzoia@gmail.com>
2025-08-08 20:54:36 -0300

    refactor(indent code)

--- server/src/Orders/presentation/routes/Order.route.ts
+++ server/src/Orders/presentation/routes/Order.route.ts
@@ -1,66 +1,105 @@
-import{
-    CreateOrderService,
-    UpdateOrderService,
-    DeleteOrderService,
-    GetAllOrdersService,
-    GetOrderByIdService,
-    GetOrdersByCustomerEmailService,
-    GetOrdersByCustomerNameService,
-    GetOrdersByCustomerPhoneService,
-    GetOrdersByProductIdService
-    
-} from '../../application/services'
+import {
+  CreateOrderService,
+  UpdateOrderService,
+  DeleteOrderService,
+  GetAllOrdersService,
+  GetOrderByIdService,
+  GetOrdersByCustomerEmailService,
+  GetOrdersByCustomerNameService,
+  GetOrdersByCustomerPhoneService,
+  GetOrdersByProductIdService,
+} from "../../application/services";
 import { PrismaOrderRepositoryImpl } from "../../infrastructure/repositories/PrismaOrder.repository.impl";
-import { ValidationMiddleware } from "../../../shared/presentation/middlewares";
-import {CreateOrderDtoSchema} from "../../domain/dtos/OrderDataDto.dto";
+import {
+  AuthMiddleware,
+  ValidationMiddleware,
+} from "../../../shared/presentation/middlewares";
+import { CreateOrderDtoSchema } from "../../domain/dtos/OrderDataDto.dto";
 import { OrderController } from "../controllers/Order.controller";
-import { Router } from 'express';
-import { OrderSendMessageToCustomer } from '../../infrastructure/adapters/OrderSendMessageToCustomer.impl';
+import { Router } from "express";
+import { OrderSendMessageToCustomer } from "../../infrastructure/adapters/OrderSendMessageToCustomer.impl";
+import { PrismaAdminRepositoryImpl } from "../../../Auth/infrastructure/repositories";
 
+export class OrderRoutes {
+  static get routes(): Router {
+    const router = Router();
+    const adminRepository = new PrismaAdminRepositoryImpl();
+    const orderRepository = new PrismaOrderRepositoryImpl();
+    const orderSendMessageToCustomer = new OrderSendMessageToCustomer();
+    const createOrderService = new CreateOrderService(
+      orderRepository,
+      orderSendMessageToCustomer
+    );
+    const updateOrderService = new UpdateOrderService(orderRepository);
+    const deleteOrderService = new DeleteOrderService(orderRepository);
+    const getAllOrdersService = new GetAllOrdersService(orderRepository);
+    const getOrderByIdService = new GetOrderByIdService(orderRepository);
+    const getOrdersByCustomerEmailService = new GetOrdersByCustomerEmailService(
+      orderRepository
+    );
+    const getOrdersByCustomerNameService = new GetOrdersByCustomerNameService(
+      orderRepository
+    );
+    const getOrdersByCustomerPhoneService = new GetOrdersByCustomerPhoneService(
+      orderRepository
+    );
+    const getOrdersByProductIdService = new GetOrdersByProductIdService(
+      orderRepository
+    );
+    const controller = new OrderController(
+      createOrderService,
+      updateOrderService,
+      deleteOrderService,
+      getAllOrdersService,
+      getOrderByIdService,
+      getOrdersByCustomerEmailService,
+      getOrdersByCustomerNameService,
+      getOrdersByCustomerPhoneService,
+      getOrdersByProductIdService
+    );
+    const authMiddleware = new AuthMiddleware(adminRepository);
 
-export class OrderRoutes{
-    static get routes():Router{
-        const router = Router();
-        const orderRepository = new PrismaOrderRepositoryImpl();
-        const orderSendMessageToCustomer = new OrderSendMessageToCustomer();
-        const createOrderService = new CreateOrderService(orderRepository, orderSendMessageToCustomer);
-        const updateOrderService = new UpdateOrderService(orderRepository);
-        const deleteOrderService = new DeleteOrderService(orderRepository);
-        const getAllOrdersService = new GetAllOrdersService(orderRepository);
-        const getOrderByIdService = new GetOrderByIdService(orderRepository);
-        const getOrdersByCustomerEmailService = new GetOrdersByCustomerEmailService(orderRepository);
-        const getOrdersByCustomerNameService = new GetOrdersByCustomerNameService(orderRepository);
-        const getOrdersByCustomerPhoneService = new GetOrdersByCustomerPhoneService(orderRepository);
-        const getOrdersByProductIdService = new GetOrdersByProductIdService(orderRepository);
-        const controller = new OrderController(
-            createOrderService,
-            updateOrderService,
-            deleteOrderService,
-            getAllOrdersService,
-            getOrderByIdService,
-            getOrdersByCustomerEmailService,
-            getOrdersByCustomerNameService,
-            getOrdersByCustomerPhoneService,
-            getOrdersByProductIdService,
-        );
+    //POSTS
+    router.post(
+      "/",
+      ValidationMiddleware.validateBody(CreateOrderDtoSchema),
+      controller.createOrder
+    );
 
+    //GETS - con autenticacion - authMiddleware.authenticate
+    router.get("/", authMiddleware.authenticate, controller.getAllOrders);
+    router.get("/:id", authMiddleware.authenticate, controller.getOrderById);
+    router.get(
+      "/customerEmail/:customerEmail",
+      authMiddleware.authenticate,
+      controller.getOrdersByCustomerEmail
+    );
+    router.get(
+      "/customerName/:customerName",
+      authMiddleware.authenticate,
+      controller.getOrdersByCustomerName
+    );
+    router.get(
+      "/customerPhone/:customerPhone",
+      authMiddleware.authenticate,
+      controller.getOrdersByCustomerPhone
+    );
+    router.get(
+      "/productId/:productId",
+      authMiddleware.authenticate,
+      controller.getOrdersByProductId
+    );
 
-        //POSTS
-        router.post('/', ValidationMiddleware.validateBody(CreateOrderDtoSchema), controller.createOrder);
-        
-        //GETS - con autenticacion - authMiddleware.authenticate
-        router.get('/', controller.getAllOrders);
-        router.get('/:id', controller.getOrderById);
-        router.get('/customerEmail/:customerEmail', controller.getOrdersByCustomerEmail);
-        router.get('/customerName/:customerName', controller.getOrdersByCustomerName);
-        router.get('/customerPhone/:customerPhone', controller.getOrdersByCustomerPhone);
-        router.get('/productId/:productId', controller.getOrdersByProductId);
-        
-        //PUT - con autenticacion - authMiddleware.authenticate
-        router.put('/:id', ValidationMiddleware.validateBody(CreateOrderDtoSchema), controller.updateOrder);
+    //PUT - con autenticacion - authMiddleware.authenticate
+    router.put(
+      "/:id",
+      authMiddleware.authenticate,
+      ValidationMiddleware.validateBody(CreateOrderDtoSchema),
+      controller.updateOrder
+    );
 
-        //DELETE - con autenticacion - authMiddleware.authenticate
-        router.delete('/:id', controller.deleteOrder);
-        return router;
-    }
+    //DELETE - con autenticacion - authMiddleware.authenticate
+    router.delete("/:id", authMiddleware.authenticate, controller.deleteOrder);
+    return router;
+  }
 }
