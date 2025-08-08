cd22b6a3473831f9ef84f404d61adc09d5d73374
ValentinZoia <valentinzoia@gmail.com>
2025-08-08 20:58:00 -0300

    refactor(update barrel file)

--- server/src/Auth/application/services/index.ts
+++ server/src/Auth/application/services/index.ts
@@ -1,3 +1,3 @@
-export * from './CreateAdmin.service';
-export * from './LogInAdmin.service';
-
+export * from "./CreateAdmin.service";
+export * from "./DeleteAdmin.service";
+export * from "./LogInAdmin.service";
