be77f9cd65b818010d6b612606e993b057180b28
ValentinZoia <valentinzoia@gmail.com>
2025-08-19 22:28:42 -0300

    feat(create loaderAuthGuard)

--- client/src/guards/auth.guard.tsx
+++ client/src/guards/auth.guard.tsx
@@ -1,23 +1,20 @@
 import { Outlet, Navigate } from "react-router-dom";
-import {PublicRoutes} from '@/types'
-import useSession from "@/hooks/useSession";
-
-
+import { PublicRoutes } from "@/types";
+import useSession from "@/hooks/Auth/useSession";
+import LoaderAuthGuard from "./LoaderAuthGuard";
 
 const PrivateValidationFragment = <Outlet />;
 
-
 export const AuthGuard = () => {
-  const {isAuthenticated, loading} = useSession();
-  if(loading){
-    return <p>Cargando...</p>
+  const { isAuthenticated, isLoading } = useSession();
+  if (isLoading) {
+    return <LoaderAuthGuard />;
   }
 
   return isAuthenticated ? (
-   
-      PrivateValidationFragment
-    )  : (
-    <Navigate replace to={`/${PublicRoutes.LOGIN}`}  />
+    PrivateValidationFragment
+  ) : (
+    <Navigate replace to={`/${PublicRoutes.LOGIN}`} />
   );
 };
 
