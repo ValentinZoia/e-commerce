20b3218f834816097951730f7ce3b9dd5acf542e
ValentinZoia <valentinzoia@gmail.com>
2025-08-19 22:23:51 -0300

    delete(unused types, utilities, shcemas, etc)

--- client/src/lib/loginSchema.ts
+++
@@ -1,8 +0,0 @@
-import {z} from 'zod';
-
-export const LoginSchema = z.object({
-  username: z.string().min(3).max(50),
-  password: z.string().min(6).max(100)
-});
-
-export type FormValues = z.infer<typeof LoginSchema>
--- client/src/types/baseTypes.ts
+++
@@ -1,5 +0,0 @@
-export interface BaseEntity {
-    id: string;
-    createdAt?: Date | string | null;
-    updatedAt?: Date | string | null;
-  }
