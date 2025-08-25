efccb8229a18b15660038c093bba6cd8bfdc1a1b
ValentinZoia <valentinzoia@gmail.com>
2025-08-24 22:45:43 -0300

    feat(add the base url api for checkout)

--- server/src/shared/presentation/routes/AppRouter.route.ts
+++ server/src/shared/presentation/routes/AppRouter.route.ts
@@ -1,29 +1,21 @@
-import { CategoryRoutes } from '../../../Categories/presentation/routes';
-import { ProductRoutes } from '../../../Products/presentation/routes';
-import { AuthAdminRoutes } from '../../../Auth/presentation/routes';
-import { OrderRoutes } from '../../../Orders/presentation/routes/Order.route';
-import { Router } from 'express';
-
-
-
+import { CategoryRoutes } from "../../../Categories/presentation/routes";
+import { ProductRoutes } from "../../../Products/presentation/routes";
+import { AuthAdminRoutes } from "../../../Auth/presentation/routes";
+import { OrderRoutes } from "../../../Orders/presentation/routes/Order.route";
+import { Router } from "express";
+import { CheckoutRoutes } from "../../../Checkout/presentation/routes";
 
 export class AppRoutes {
-
-
   static get routes(): Router {
-
     const router = Router();
 
     // Definir todas mis rutas principales
-    router.use('/api/admin', AuthAdminRoutes.routes )
-    router.use('/api/products', ProductRoutes.routes)
-    router.use('/api/categories', CategoryRoutes.routes)
-    router.use('/api/orders', OrderRoutes.routes)
-    
-
+    router.use("/api/admin", AuthAdminRoutes.routes);
+    router.use("/api/products", ProductRoutes.routes);
+    router.use("/api/categories", CategoryRoutes.routes);
+    router.use("/api/orders", OrderRoutes.routes);
+    router.use("/api/checkout", CheckoutRoutes.routes);
 
     return router;
   }
-
-
 }
